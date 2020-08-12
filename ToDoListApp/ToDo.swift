674846f5e02deae14cbe44c19b8a3ace59205d9e
maddieshenkan <maddieshenkan@gmail.com>
2020-08-12 11:33:20 -0700

    trying to commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Maddie Shenkan on 8/10/20.
+//  Copyright © 2020 Maddie Shenkan. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+}
+
